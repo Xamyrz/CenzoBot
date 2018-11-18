40d52d00554f9924050ad14d85c1300b92f4c17a
Grafitter <3stylemylife@gmail.com>
2018-11-18 20:39:47 +0100

    Add message debug

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const Commando = require('discord.js-commando');
+const Discord = require("discord.js");
 const config = require('./config.json');
-const client = new Commando.Client();
+const client = new Discord.Client();
 const fs = require('file-system');
 
 console.log("CenzoBot is running");
-
 client.login(config.token);
+
+client.on("message", async message => {
+    if(message.author.bot) return;
+    console.log(message);
+});
