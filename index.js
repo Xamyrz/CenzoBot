5f37645e67e837085feec5b230708c122f7d1640
BuildTools <unconfigured@null.spigotmc.org>
2018-11-18 18:58:59 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const Commando = require('discord.js-commando');
+const fs = require('file-system');
+const client = new Commando.Client();
+
+console.log("CenzoBot is running");
+
+client.login('NTExMTU4MzEwNDQwMDA5NzMw.DstIzw.L6aKNC2-QkQiV40i5QcLrpixG00');
